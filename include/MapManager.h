8a802e29061a4bc9331f42bbf08dc326862a11b6
NotShadex <maks.brolih@gmail.com>
2026-02-21 11:32:55 +0100

    Handles the drawing of the map and can return positions of entities inside it

---
+++ include/MapManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SDL.h>
+#include <vector>
+#include <glm/glm.hpp>
+#include "Globals.h"
+#include "ResourceManager.h"
+
+class MapManager {
+    private:
+        int mapWidth, mapHeight;
+        float cellSize;
+        std::vector<std::vector<int>> grid;
+        std::vector<Sprite> tileSet;
+    public:
+        MapManager(int width = MAP_WIDTH, int height = MAP_HEIGHT, float cellSize = CELL_SIZE);
+        void LoadTileset(const std::string& name, int count);
+        int GetTile(float worldX, float worldY);
+        void Draw(SDL_Renderer* renderer, glm::vec2 cam);
+};
