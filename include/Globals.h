63087b54dec746c1d714d7dbf07fb52e2b4d8a5a
NotShadex <maks.brolih@gmail.com>
2026-02-22 21:12:27 +0100

    New variables so you can somewhat understand what is happening in this clusterfuck.

--- include/Globals.h
+++ include/Globals.h
@@ -3,9 +3,18 @@
 #define WIDTH 1280
 #define HEIGHT 720
 // MAP
-#define CELL_SIZE 32.0f
+#define CELL_SIZE 32
 #define MAP_WIDTH 40
 #define MAP_HEIGHT 23
 // CAMERA
-#define ZOOM_SPEED 5.0f
+#define BASE_ZOOM 2.0f
+#define ZOOM_SPEED 5
+#define SIGHT_RADIUS 150
+// PLAYER
+#define WALK_SPEED 200.0f
+#define SHIP_SPEED 300.0f
+// ENEMIES
+#define FEAR_RADIUS 100
+#define BASE_SPEED 80
+#define DROP_COOLDOWN 10
 
