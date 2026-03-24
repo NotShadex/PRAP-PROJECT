c7a16090789d256c3d61b150ad5bad70ab5d6f9d
NotShadex <maks.brolih@gmail.com>
2026-03-24 18:11:04 +0100

    Added the GameState to globals and the new enum DIRECTIONS that the Enemy and Player share for sprite drawing

--- include/Globals.h
+++ include/Globals.h
@@ -1,6 +1,14 @@
 #pragma once
+// GAME STATES
+enum GameState 
+{
+    MENU,
+    GAME,
+    QUIT,
+    SETTINGS
+};
 // SCREEN
-#define WIDTH 1280
+#define WIDTH 1280 // better name would be GAME_WIDTH but I can't be bothered to refactor everything...
 #define HEIGHT 720
 // MAP
 #define CELL_SIZE 32
@@ -13,13 +21,22 @@
 // PLAYER
 #define WALK_SPEED 200.0f
 #define SHIP_SPEED 300.0f
+enum DIRECTIONS {
+    DOWN_LEFT,
+    DOWN,
+    DOWN_RIGHT,
+    RIGHT,
+    UP_RIGHT,
+    UP,
+    UP_LEFT,
+    LEFT,
+};
 // ENEMIES
 #define FEAR_RADIUS 100
 #define ENEMY_BASE_SPEED 100.0f
 #define DROP_COOLDOWN 7
 #define ENEMY_POINTS 50
 // TRASH
-#define TRASH_BASE_SPEED 60.0f
-#define TRASH_POINTS 10
-#define TRASH_POINTS_PENALTY 100
-
+#define TRASH_BASE_SPEED 50.0f
+#define TRASH_POINTS 20
+#define TRASH_POINTS_PENALTY 30
