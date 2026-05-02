a06b244945c13fe5ab84bba99b51d15098f0b002
NotShadex <maks.brolih@gmail.com>
2026-05-02 17:21:13 +0200

    Added best score function for later implementation if neccessary.

--- include/ScoreManager.h
+++ include/ScoreManager.h
@@ -4,8 +4,14 @@ class ScoreManager
 {
 private:
     static inline int score = 0; // did "inline" so there is no need to create a .cpp file
+    static inline int bestScore = 0;
 public:
-    static void Add(int amount) { score += amount; }
+    static void Add(int amount) { 
+        score += amount;  
+        if (bestScore < score) bestScore = score;
+    }
     static int Get() { return score; }
+    static int GetBest() { return bestScore; }
+    static int SetBest(int amount) { bestScore = amount; }
     static void Reset() { score = 0; }
 };
